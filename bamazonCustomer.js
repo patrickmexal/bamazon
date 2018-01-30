8d530c0833c6179360f68fdb1a515490d8294be9
Patrick Mexal <patrickmexal@gmail.com>
2018-01-30 10:43:43 -0700

    update six

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,10 +1,18 @@
 var mysql = require("mysql");
 var prompt = require("prompt");
 var table = require("cli-table");
+var productsPurchased = [];
 
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
     database: "bamazon", 
+});
+
+connection.connect();
+
+connection.query('SELECT item_id, product_name, department_name, price FROM products', function(err, result){
+    if(err) console.log(err);
+    else console.log(result);
 });
