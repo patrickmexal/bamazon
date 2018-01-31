8abf8ceae83971c5fee51211730ea5e1562e2429
Patrick Mexal <patrickmexal@gmail.com>
2018-01-31 16:28:18 -0700

    update ten

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 var prompt = require("prompt");
 var inquirer = require("inquirer");
-var desiredPurchase = "";
-var desiredQuantity = 0;
+var desiredPurchase;
+var desiredQuantity;
 
 
-/* Two Functions to run the CLI inquiry */
+/* Function to run the CLI inquiry */
 function inquireForPurchase() {
 
     inquirer.prompt([ {
@@ -21,7 +21,7 @@ function inquireForPurchase() {
         choices: ""
     },
     ]).then(function(answers) {
-          var desiredPurchase = answers.whatDoYouWant;
+          var desiredPurchase = JSON.stringify(answers.whatDoYouWant);
           var desiredQuantity = answers.howManyDoYouWant;
           bamazonPurchase(desiredPurchase, desiredQuantity);
     });
@@ -45,12 +45,11 @@ connection.query('SELECT item_id, product_name, department_name, price, stock_qu
     //else console.log(result);
 });
 
-console.log(stock_quantity);
 /* MySQL Purchase Query */
 function bamazonPurchase(desiredQuantity, desiredPurchase) {
     connection.query("SELECT * FROM products WHERE product_name" + desiredPurchase, function(error, response) {
         if (desiredQuantity <= response[0].stock_quantity) {
-            console.log("On the way my friend!");
+            console.log("That is on the way, my friend!");
         } else {
             console.log("Sorry, we don't have the quantity to fill that order.")
         };
