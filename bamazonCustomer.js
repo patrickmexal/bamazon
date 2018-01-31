30880d9a3639bad0d957feef4f622ec81fd07f33
Patrick Mexal <patrickmexal@gmail.com>
2018-01-30 19:07:58 -0700

    update ten

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,38 +1,37 @@
+/* Set Global Variables */
 var mysql = require("mysql");
 var prompt = require("prompt");
 var inquirer = require("inquirer");
 var table = require("cli-table");
-var productsPurchased = [];
+var desiredPurchase = "";
+var desiredQuantity = 0;
 
 
-
+/* Two Functions to run the CLI inquiry */
 function inquireForPurchase() {
 
-    inquirer.prompt({
+    inquirer.prompt([ {
         name: "whatDoYouWant",
         message: "What would you like to buy today?",
         type: "list",
         choices: ["Macbook Pro", "Sony A7", "Timex Weekender Chronograph", "Iphone X", "Google Pixel", "Sony A6000", "Fuji X-T2", "Macbook Air", "Sony NEX-5N", "Apple Watch"]
-    }).then(function(answers) {
-        let desiredPurchase = answers.whatDoYouWant;
-    });
-};
-
-function inquireForQuantity() {
-
-    inquirer.prompt({
+    }, {
         name: "howManyDoYouWant",
         message: "How many of your chosen item do you wish to buy?",
         type: "input",
-        choices: []
-    }).then(function(answers) {
-        let desiredQuantity = answers.howManyDoYouWant;
+        choices: ""
+    },
+    ]).then(function(answers) {
+          var desiredPurchase = answers.whatDoYouWant;
+          var desiredQuantity = answers.howManyDoYouWant;
+          bamazonPurchase(desiredPurchase, desiredQuantity);
     });
 };
 
-inquireForPurchase();
-inquireForQuantity();
 
+inquireForPurchase();
+
+/* MySQL connection */
  var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -46,3 +45,18 @@ connection.query('SELECT item_id, product_name, department_name, price, stock_qu
     if (err) console.log(err);
     //else console.log(result);
 });
+
+/* MySQL Purchase Query */
+function bamazonPurchase() {
+    connection.query("SELECT * FROM products WHERE product_name" + desiredPurchase, function(error, response) {
+        if (desiredQuantity <= response[0].stock_quantity) {
+            console.log("On the way my friend!");
+        } else {
+            console.log("Sorry, we don't have the quantity to fill that order.")
+        };
+    });
+};
+
+
+
+
