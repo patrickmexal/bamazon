8e0a7814c329a684ea046d73c797aebea05b7cea
Patrick Mexal <patrickmexal@gmail.com>
2018-01-31 16:55:12 -0700

    update twelve

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -42,7 +42,7 @@ connection.connect();
 
 connection.query('SELECT item_id, product_name, department_name, price, stock_quantity FROM products', function(err, result) {
     if (err) console.log(err);
-    else console.log(result);
+    //else console.log(result);
 });
 
 /* MySQL Purchase Query */
